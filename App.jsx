aa6d57cc1e07942032c15b6adccb132fa05949ef
Lincoln <lincolnnovais15a@gmail.com>
2024-02-05 10:50:39 -0300

    Add documentação para a página App.jsx

--- App.jsx
+++ App.jsx
@@ -1,8 +1,12 @@
+// Importa a biblioteca React do pacote 'react'
 import React from "react";
 
+// Importa o componente 'RenderPosts' de um arquivo chamado 'Posts' na pasta 'components' dentro do diretório 'src'
 import RenderPosts from './src/components/Posts';
 
+// Função principal do componente App
 function App() {
+    // Retorna o JSX que compõe o componente
     return (
         <>
             <RenderPosts />
@@ -10,4 +14,5 @@ function App() {
     );
 }
 
+// Exporta o componente App para ser utilizado em outros arquivos
 export default App;
